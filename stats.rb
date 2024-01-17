4d95ac39707eb3d8798e36ecdcbc5a536722bc44
alexzavalny <alex@zavalny.com>
2024-01-17 22:56:20 +0200

    fix calculations

--- stats.rb
+++ stats.rb
@@ -20,14 +20,6 @@ def fetch_day_stats(username, date)
 
   month_games = response['games']
   today_stats = { played: 0, won: 0, lost: 0, draw: 0, types: [], total_time: 0 }
-  result_mapping = {
-    'win' => 'won', 'checkmated' => 'lost', 'agreed' => 'draw', 
-    'repetition' => 'draw', 'timeout' => 'lost', 'resigned' => 'lost', 
-    'stalemate' => 'draw', 'lose' => 'lost', 'insufficient' => 'draw', 
-    '50move' => 'draw', 'abandoned' => 'lost', 'kingofthehill' => 'lost', 
-    'threecheck' => 'lost', 'timevsinsufficient' => 'draw', 
-    'bughousepartnerlose' => 'lost'
-  }
 
   month_games.each do |game|
     game_date = Time.at(game['end_time']).to_date
@@ -37,19 +29,29 @@ def fetch_day_stats(username, date)
     today_stats[:types] << game['time_class']
     today_stats[:total_time] += game['time_control'].to_i
 
-    result_category = game_result(game, username, result_mapping).to_sym
+    result_category = game_result(game, username).to_sym
     today_stats[result_category] += 1 if today_stats.key?(result_category)
   end
 
   today_stats
 end
 
-def game_result(game, username, result_mapping)
+def game_result(game, username)
+  result_mapping = {
+    'win' => 'won', 'checkmated' => 'lost', 'agreed' => 'draw', 
+    'repetition' => 'draw', 'timeout' => 'lost', 'resigned' => 'lost', 
+    'stalemate' => 'draw', 'lose' => 'lost', 'insufficient' => 'draw', 
+    '50move' => 'draw', 'abandoned' => 'lost', 'kingofthehill' => 'lost', 
+    'threecheck' => 'lost', 'timevsinsufficient' => 'draw', 
+    'bughousepartnerlose' => 'lost'
+  }
+
   if game['white']['username'].downcase == username.downcase
     result = game['white']['result']
   else
     result = game['black']['result']
   end
+
   result_mapping[result] || 'unknown'
 end
 
@@ -72,10 +74,10 @@ end
 
 puts " "
 
-puts "Yesterday Stats:"
-usernames.each do |username|
-  stats = fetch_yesterday_stats(username)
-  total_time_formatted = format_duration(stats[:total_time])
-  puts "#{username}: Played: #{stats[:played]}, Won: #{stats[:won]}, Lost: #{stats[:lost]}, Draw: #{stats[:draw]}, Types: #{stats[:types].uniq}, Total Time: #{total_time_formatted}"
-  puts
-end
+# puts "Yesterday Stats:"
+# usernames.each do |username|
+#   stats = fetch_yesterday_stats(username)
+#   total_time_formatted = format_duration(stats[:total_time])
+#   puts "#{username}: Played: #{stats[:played]}, Won: #{stats[:won]}, Lost: #{stats[:lost]}, Draw: #{stats[:draw]}, Types: #{stats[:types].uniq}, Total Time: #{total_time_formatted}"
+#   puts
+# end
