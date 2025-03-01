8e1976e95d6beea04bf52a82d2fe85f2ec07b7ee
alexzavalny <alex@zavalny.com>
2025-03-01 21:20:58 +0200

    Ksjuwa

--- config.js
+++ config.js
@@ -5,8 +5,15 @@ export const CONFIG = {
         jefimserg: "Sergey",
         TheErix: "Erik",
         vadimostapchuk: "Vadim",
+        KsenijaTojeckina: "Ksjuwa",
     },
-    DEFAULT_USERNAMES: ["sonicspeedmate", "jefimserg", "TheErix", "vadimostapchuk"],
+    DEFAULT_USERNAMES: [
+        "sonicspeedmate",
+        "jefimserg",
+        "TheErix",
+        "vadimostapchuk",
+        "KsenijaTojeckina",
+    ],
     GAME_TYPE_MAP: {
         chess_blitz: "blitz",
         chess_rapid: "rapid",
@@ -14,5 +21,5 @@ export const CONFIG = {
         tactics: "tactics",
         puzzle_rush: "puzzle_rush",
     },
-    API_BASE_URL: "https://api.chess.com/pub"
-}; 
+    API_BASE_URL: "https://api.chess.com/pub",
+};
